8640cdbc841ce2473dfb5dd29177e2bdbf37daa9
Şeyda Nur Sert <seydaanur.sert@gmail.com>
2025-07-23 16:18:10 +0300

    Add files via upload
    zaten diğerlerinde mylib adında senin kodlarını kullandım  bir tek mylib içinde init.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .db_connector import DBConnector
+from .tablemanager import TableManager
+from .comparator import Comparator  # varsa
+
+__all__ = ["DBConnector", "TableManager", "Comparator"]
--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pydbcontrol",
+    name="mylib",
     version="0.1.0",
-    packages=["pydbcontrol"],
+    packages=find_packages(),
     install_requires=[
-        'psycopg2-binary',
-        'python-dotenv'
-    ],
-    author="Ediz Arkin Kobak",
-    author_email="arkinediz@gmail.com",
-    description="A Python library to control and compare PostgreSQL tables",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/EdizArkin/TUBITAK-UZAY-PyDBControl",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
+        "psycopg2-binary",
+        "python-dotenv",
+        "pandas",
     ],
     python_requires=">=3.7",
+    author="Senin İsmin",
+    description="Generic veritabanı API kütüphanesi",
 )
+
