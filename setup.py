795e8ebbd7117688c93c0e05a749d4df19d16465
Şeyda Nur Sert <seydaanur.sert@gmail.com>
2025-07-23 11:46:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
-from setuptools import setup, find_packages
 
-setup(
-    name="pydbcontrol",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=[
-        "psycopg2",
-    ],
-    author="Ediz Arkin",  # geliştirici adi
-    author_email="",  # maili
-    description="Veritabanı için işlevsel bir API",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/EdizArkin/TUBITAK-UZAY-PyDBControl",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
-)
+
+
